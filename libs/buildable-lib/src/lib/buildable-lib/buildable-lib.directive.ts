fc2c460ed10492dfb6f58367fea19bde4e1bd7c0
Daniel Holmes <daniel.holmes@satac.edu.au>
2025-04-21 08:23:00 +0930

    building with text from txt file

--- libs/buildable-lib/src/lib/buildable-lib/buildable-lib.directive.ts
+++ libs/buildable-lib/src/lib/buildable-lib/buildable-lib.directive.ts
@@ -1,10 +1,11 @@
 import { Directive } from '@angular/core';
+import { HELLO_TEXT } from './hello-text';
 
 @Directive({
   selector: 'div[libBuildableLib]',
 })
 export class BuildableLibDirective {
   constructor() {
-    console.log('hello world');
+    console.log(HELLO_TEXT);
   }
 }
